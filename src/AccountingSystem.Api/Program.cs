ddc870b4f15a3239ddeb87114517eef8f2a62770
saugat magar <saugatmagar126@gmail.com>
2026-09-07 14:58:25 +0545

    changes

--- src/AccountingSystem.Api/Program.cs
+++ src/AccountingSystem.Api/Program.cs
@@ -1,7 +1,10 @@
 using AccountingSystem.Application.Interfaces;
 using AccountingSystem.Application.Services;
 using AccountingSystem.Infrastructure.SQLRepo;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,13 +19,44 @@ builder.Services.AddScoped<CompanyService>();
 builder.Services.AddScoped<AccountService>();
 builder.Services.AddScoped<UserService>();
 
+// --- JWT Authentication ---
+var jwtKey = builder.Configuration["Jwt:Key"]!;
+var jwtIssuer = builder.Configuration["Jwt:Issuer"]!;
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = jwtIssuer,
+        ValidAudience = jwtIssuer,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey))
+    };
+});
+
+builder.Services.AddAuthorization();
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("WebApp", policy =>
-        policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+        policy.WithOrigins(
+                "https://accountmanagement.saugatrana126.workers.dev",
+                "http://localhost:5173"
+              )
+              .AllowAnyHeader()
+              .AllowAnyMethod());
 });
 
 var app = builder.Build();
@@ -31,6 +65,8 @@ app.UseSwagger();
 app.UseSwaggerUI();
 
 app.UseCors("WebApp");
+app.UseAuthentication();
+app.UseAuthorization();
 app.MapControllers();
 
 app.MapGet("/health", () => Results.Ok("API is alive"));
